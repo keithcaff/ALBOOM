a78581bbe0e9228fde49bc094b27ae5775a18868
Keith Caffrey <keithcaffrey@gmail.com>
2016-08-06 14:57:30 +0100

    updating EAHomecell, home VC to display one cell one the screen at a time and forcing app to ony display in portrait mode

--- EAHomeViewController.swift
+++ EAHomeViewController.swift
@@ -57,72 +57,13 @@ public class EAHomeViewController: UIViewController,UITableViewDelegate, UITable
         // Dispose of any resources that can be recreated.
     }
     
-    // MARK:Tableview delegates
-    
-//    public func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-//        print("selected row \(indexPath.row)")
-//    }
-//    
-    public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        var count:Int = 0
-        if let data = currentFiles?.files {
-            count = data.count
-        }
-        return count;
-    }
-
-    public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        var cell:EAHomeTableViewCell?
-        if (self.currentFiles?.files == nil || self.currentFiles!.files.count < 1){
-            return EAHomeTableViewCell()
-        }
-        
-        let currentFiles:NSArray? = self.currentFiles?.files
-        var file:GTLDriveFile?
-        if let files = currentFiles{
-            file = (files.objectAtIndex(indexPath.row) as! GTLDriveFile)
-        }
-        
-        cell = tableView.dequeueReusableCellWithIdentifier(homeCellIdentifier, forIndexPath: indexPath) as? EAHomeTableViewCell
-//        if let name:String = file?.name {
-//            cell?.imageTitleLabel.text = name
-//        }
-//        let bgImage:UIImage! = UIImage(named: "jake.jpg")
-//        bgImage.resizableImageWithCapInsets(UIEdgeInsetsMake(0,5,40,1), resizingMode: UIImageResizingMode.Stretch)
-//        let imageView:UIImageView = UIImageView(image: bgImage)
-//        cell?.backgroundView = imageView
-        
-        
-        
-//        UIImage(named: "jake.jpg" )?.drawInRect(self.view.bounds)
-//        
-//        var image: UIImage = UIGraphicsGetImageFromCurrentImageContext()
-//        image = image.resizableImageWithCapInsets(UIEdgeInsetsMake(0,5,40,1), resizingMode: UIImageResizingMode.Stretch)
-
-        
-        addBackground(cell!.placeHolderView)
-        
-    
-        
-        //cell?.backgroundView = imageView
-        //bgImage.resizingMode = UIImageResizingMode.Stretch
-  
-//        cell?.contentImageView.contentMode = UIViewContentMode.ScaleAspectFill;
-//        cell?.contentImageView.clipsToBounds = true;
-        
-        return cell!
-    }
-    
-    
     func addBackground(view:UIView) {
+        
         // screen width and height:
         let width = view.bounds.size.width
         let height = view.bounds.size.height
         
         let imageViewBackground = UIImageView(frame: CGRectMake(0, 0, width, height))
-        
-//        let bgImage:UIImage! = UIImage(named: "jake.jpg")
-//        bgImage.resizableImageWithCapInsets(UIEdgeInsetsMake(0,5,width,height), resizingMode: UIImageResizingMode.Stretch)
         imageViewBackground.image = UIImage(named: "jake.jpg")
         
         // you can change the content mode:
@@ -131,7 +72,60 @@ public class EAHomeViewController: UIViewController,UITableViewDelegate, UITable
         view.addSubview(imageViewBackground)
         view.sendSubviewToBack(imageViewBackground)
     }
+    
+    // MARK:Tableview delegates
+    
+    public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        var count:Int = 0
+        if let data = currentFiles?.files {
+            count = data.count
+        }
+        return count;
+    }
+    
+    public func tableView(tableView: UITableView,willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        let homeCell:EAHomeTableViewCell = (cell as! EAHomeTableViewCell)
+        //FYI:Adding background image here because the placeHolderview.frame.bounds seem to be incorrect on first call to cell for row
+        for view in homeCell.placeHolderView.subviews{
+            view.removeFromSuperview()
+        }
+        addBackground(homeCell.placeHolderView)
+    }
 
+    public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell:EAHomeTableViewCell?
+        if (self.currentFiles?.files == nil || self.currentFiles!.files.count < 1){
+            return EAHomeTableViewCell()
+        }
+        
+        let currentFiles:NSArray? = self.currentFiles!.files
+        var file:GTLDriveFile?
+        if let files = currentFiles{
+            file = (files.objectAtIndex(indexPath.row) as! GTLDriveFile)
+        }
+        
+        cell = tableView.dequeueReusableCellWithIdentifier(homeCellIdentifier, forIndexPath: indexPath) as? EAHomeTableViewCell
+        
+        //        if let name:String = file?.name {
+        //            cell?.imageTitleLabel.text = name
+        //        }
+        //        let bgImage:UIImage! = UIImage(named: "jake.jpg")
+        //        bgImage.resizableImageWithCapInsets(UIEdgeInsetsMake(0,5,40,1), resizingMode: UIImageResizingMode.Stretch)
+        //        let imageView:UIImageView = UIImageView(image: bgImage)
+        //        cell?.backgroundView = imageView
+        
+        return cell!
+    }
+    
+    
+    public func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat
+    {
+        let topNavHeight:CGFloat = self.navigationController!.navigationBar.frame.height
+        let bottomNavHeight:CGFloat =  self.tabBarController!.tabBar.frame.height
+        let statusBarHeight:CGFloat = UIApplication.sharedApplication().statusBarFrame.size.height
+        let navHeight:CGFloat = topNavHeight+bottomNavHeight+statusBarHeight
+        return self.tableView.frame.height-navHeight
+    }
     
     // MARK:notifaction responses/selectors
     func newEventCreated(notifiaction : NSNotification) {
@@ -150,8 +144,7 @@ public class EAHomeViewController: UIViewController,UITableViewDelegate, UITable
         revealViewController().revealToggleAnimated(true)
         if let files = notifiaction.object as? GTLDriveFileList  {
             currentFiles = files
-            self.tableView.reloadData()//:CGPointZer
-          // self.tableView.setContentOffset(CGPointZero,animated: true)
+           self.tableView.reloadData()
         }
     }
 
@@ -159,5 +152,6 @@ public class EAHomeViewController: UIViewController,UITableViewDelegate, UITable
     deinit {
         NSNotificationCenter.defaultCenter().removeObserver(self)
     }
+    
     
 }
