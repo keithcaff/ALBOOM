15c78ace57c010980f13b133508988a29b6b77f1
Keith Caffrey <keithcaffrey@gmail.com>
2017-07-31 17:27:34 +0100

    fixing UI activity indicator

--- EAHomeViewController.swift
+++ EAHomeViewController.swift
@@ -181,20 +181,20 @@ open class EAHomeViewController: UIViewController,UITableViewDelegate, UITableVi
         let container: UIView = UIView()
         container.frame = uiView.frame
         container.center = uiView.center
-        container.backgroundColor = UIColor(white: 0xffffff, alpha: 0.3)
+        container.backgroundColor = UIColor(hex: 0xffffff, alpha: 0.3)
         let loadingView: UIView = UIView()
-        loadingView.frame = CGRect(x: 0, y: 0, width: 80, height: 80)
+        loadingView.frame = CGRect(x:0, y:0, width:80, height:80)
         loadingView.center = uiView.center
-        loadingView.backgroundColor = UIColor(white: 0x444444, alpha: 0.7)
+        loadingView.backgroundColor = UIColor(hex: 0x444444, alpha: 0.7)
         loadingView.clipsToBounds = true
         loadingView.layer.cornerRadius = 10
         
         let actInd: UIActivityIndicatorView = UIActivityIndicatorView()
-        actInd.frame = CGRect(x: 0.0, y: 0.0, width: 80.0, height: 80.0);
+        actInd.frame = CGRect(x:0.0, y:0.0, width:80.0, height:80.0);
         actInd.activityIndicatorViewStyle =
             UIActivityIndicatorViewStyle.whiteLarge
-        actInd.center = CGPoint(x: loadingView.frame.size.width / 2,
-                                    y: loadingView.frame.size.height / 2);
+        actInd.center = CGPoint(x:loadingView.frame.size.width / 2,
+                                y:loadingView.frame.size.height / 2);
         loadingView.addSubview(actInd)
         container.addSubview(loadingView)
         uiView.addSubview(container)
